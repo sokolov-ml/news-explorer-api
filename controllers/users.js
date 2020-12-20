27f27ea49fe3d77c1960e575aed8c50abf6a82bf
Mikahil Sokolov <pilligrim17@yandex.ru>
2020-12-20 23:53:37 +0300

    added dotenv

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 // const getUsers = (req, res, next) => {
 //   User.find({})
 //     .then((data) => {
@@ -66,7 +68,11 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+        { expiresIn: '7d' },
+      );
       res.send({ token });
     })
     .catch(next);
